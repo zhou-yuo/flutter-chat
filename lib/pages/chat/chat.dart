ebf3e9a0d4e61c5a26b2b1bdfe1b5892e8b499ac
xiayu <xiayu649@gmail.com>
2024-07-09 10:32:03 +0700

    chat

--- lib/pages/chat/chat.dart
+++ lib/pages/chat/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -14,11 +15,77 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(
         title: const Text('Chat'),
       ),
-      body: const SafeArea(
-        child: Center(
-          child: Text('Chat Page'),
-        ),
+      body: SafeArea(
+        child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+            ),
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Center(
+                    child: Text('page view'),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(12))),
+                  child: Column(
+                    children: [
+                      sendCtrl(),
+                    ],
+                  ),
+                )
+              ],
+            )),
       ),
+    );
+  }
+
+  Widget sendCtrl() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: TextField(
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            // textInputAction: TextInputAction.go,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              filled: true,
+              fillColor: Colors.grey[100],
+            ),
+
+            style: const TextStyle(
+              fontSize: 14,
+              color: Colors.black87,
+            ),
+          ),
+        ),
+        Container(
+          width: 32,
+          height: 32,
+          margin: const EdgeInsets.only(left: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.blue,
+          ),
+          child: const Center(
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+              size: 18,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
