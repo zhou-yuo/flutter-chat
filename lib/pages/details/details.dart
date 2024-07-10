e5cb9dbd113d1a14fdc5b2f1bf8b17307dc6ddc5
xiayu <xiayu649@gmail.com>
2024-07-10 18:46:05 +0700

    detail

--- lib/pages/details/details.dart
+++ lib/pages/details/details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
 
 class ContactDetailsPage extends StatefulWidget {
   const ContactDetailsPage({super.key});
@@ -23,7 +25,10 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
             children: [
               sectionCard(userinfoWidget()),
               sectionCard(Column(
-                children: cellItems(),
+                children: cellItems(_powerList),
+              )),
+              sectionCard(Column(
+                children: cellItems(_handleList, center: true),
               )),
             ],
           ),
@@ -88,68 +93,109 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
     );
   }
 
-  final List _cellList = [
+  final List _powerList = [
     {'id': 'query', 'label': '查找聊天记录'},
     {'id': 'remark', 'label': '设置备注'},
     {'id': 'mute', 'label': '消息免打扰'},
     {'id': 'top', 'label': '置顶聊天'},
   ];
 
-  List<Widget> cellItems() {
+  final List _handleList = [
+    {'id': 'chat', 'label': '发送消息'},
+    {'id': 'delete', 'label': '删除好友'},
+  ];
+
+  List<Widget> cellItems(List cellList, {bool? center}) {
     List<Widget> items = [];
-    for (int i = 0; i < _cellList.length; i++) {
-      items.add(Container(
-        height: 46,
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 4),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: i + 1 == _cellList.length
-                ? BorderSide.none
-                : BorderSide(width: 1, color: Colors.grey.shade200),
+    for (int i = 0; i < cellList.length; i++) {
+      final item = cellList[i];
+      items.add(
+        InkWell(
+          onTap: () {
+            debugPrint('onTap');
+            cellItemTap(item);
+          },
+          child: Container(
+            height: 46,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 4),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: i + 1 == cellList.length
+                    ? BorderSide.none
+                    : BorderSide(width: 1, color: Colors.grey.shade200),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: center == true
+                  ? MainAxisAlignment.center
+                  : MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  item['label'],
+                  style: TextStyle(
+                    color: item['id'] == 'delete' ? Colors.red : Colors.black,
+                  ),
+                ),
+                Offstage(
+                  offstage: center == true,
+                  child: item['id'] == 'mute'
+                      ? muteSwitch()
+                      : item['id'] == 'top'
+                          ? topSwitch()
+                          : Icon(
+                              Icons.arrow_forward_ios_rounded,
+                              size: 18,
+                              color: Colors.grey.shade400,
+                            ),
+                ),
+              ],
+            ),
           ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(_cellList[i]['label']),
-            _cellList[i]['id'] == 'mute'
-                ? muteSwitch()
-                : _cellList[i]['id'] == 'top'
-                    ? topSwitch()
-                    : Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        size: 18,
-                        color: Colors.grey.shade400,
-                      ),
-          ],
-        ),
-      ));
+      );
     }
     return items;
   }
 
+  cellItemTap(item) {
+    debugPrint('cellItemTap, ${item['id']}');
+    switch (item['id']) {
+      case 'chat':
+        context.push('/chat');
+        break;
+    }
+  }
+
   bool muteFlag = false;
   Widget muteSwitch() {
-    return Switch(
-      value: muteFlag,
-      onChanged: (bool value) {
-        setState(() {
-          muteFlag = value;
-        });
-      },
+    return Transform.scale(
+      scale: 0.8,
+      child: CupertinoSwitch(
+        value: muteFlag,
+        activeColor: Theme.of(context).primaryColor,
+        onChanged: (bool value) {
+          setState(() {
+            muteFlag = value;
+          });
+        },
+      ),
     );
   }
 
   bool topFlag = false;
   Widget topSwitch() {
-    return Switch(
-      value: topFlag,
-      onChanged: (bool value) {
-        setState(() {
-          topFlag = value;
-        });
-      },
+    return Transform.scale(
+      scale: 0.8,
+      child: CupertinoSwitch(
+        value: topFlag,
+        activeColor: Theme.of(context).primaryColor,
+        onChanged: (bool value) {
+          setState(() {
+            topFlag = value;
+          });
+        },
+      ),
     );
   }
 }
